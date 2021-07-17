536e96697ffae03b8efc1f03276970887400d5bf
rudevenger <rudevenger@users.noreply.github.com>
2021-07-17 17:00:43 +0000

    deploy: 7e004e4d7268137b16c67fd59e7c5adf4505868c

---
+++ _nuxt/1cbc0dd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{117:function(n,o,w){n.exports=w(118)}},[[117,5,1,6]]]);
---
+++ _nuxt/static/1626541243/more-fun/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/more-fun", {data:[{}],fetch:{},mutations:void 0});
